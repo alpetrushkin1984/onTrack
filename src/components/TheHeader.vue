943bb343512aaad7da97d70576da576415b2e12a
alpetrushkin <alpetrushkin1984@gmail.com>
2024-09-30 08:22:20 +0300

    header and nav component

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,14 @@
+<script setup>
+import TheLogo from '@/components/TheLogo.vue'
+import TheHeaderProgress from '@/components/TheHeaderProgress.vue'
+</script>
+
+<template>
+    <header
+        class="sticky top-0 z-20 bg-white border-b p-3 flex justify-between items-center"
+    >
+        <TheLogo />
+
+        <TheHeaderProgress />
+    </header>
+</template>
